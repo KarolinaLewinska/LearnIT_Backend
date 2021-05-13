0d86234ed5d623274e7157ab6847c24ade9e5712
KarolinaLewinska <lewinskak95@gmail.com>
2021-05-13 13:09:29 +0200

    JSON format

--- LearnIT/App_Start/WebApiConfig.cs
+++ LearnIT/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -19,6 +20,11 @@ namespace LearnIT
                 routeTemplate: "learn-it/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            var jsonFormatter = config.Formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            jsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
         }
     }
 }
--- LearnIT/Controllers/NotesController.cs
+++ LearnIT/Controllers/NotesController.cs
@@ -17,71 +17,194 @@ namespace LearnIT.Controllers
         [HttpGet]
         public IHttpActionResult GetAllNotes()
         {
-            return Ok(_dbContext.Notes.ToList());
+            try
+            {
+                var notesList = _dbContext.Notes.ToList();
+                if (notesList == null)
+                {
+                    var response = new HttpResponseMessage(HttpStatusCode.NotFound)
+                    {
+                        Content = new StringContent("Brak danych w bazie", System.Text.Encoding.UTF8, "text/plain"),
+                        StatusCode = HttpStatusCode.NotFound
+                    };
+                    throw new HttpResponseException(response);
+                    //return NotFound();
+                }
+                if (!ModelState.IsValid)
+                {
+                    var response = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                    {
+                        Content = new StringContent("Nieprawidłowe żądanie", System.Text.Encoding.UTF8, "text/plain"),
+                        StatusCode = HttpStatusCode.BadRequest
+                    };
+                    throw new HttpResponseException(response);
+                    //return BadRequest("Nieprawidłowe żądanie");
+                }
+
+                return Ok(notesList);
+            }
+            catch(Exception exc)
+            {
+                throw exc;
+            }
         }
 
         [Route("zagadnienie/{id}")]
         [HttpGet]
         public IHttpActionResult GetNote([FromUri] int id)
         {
-            return Ok(_dbContext.Notes.Find(id));
+            try
+            {
+                var chosenNote = _dbContext.Notes.Find(id);
+                if (chosenNote == null)
+                {
+                    var response = new HttpResponseMessage(HttpStatusCode.NotFound)
+                    {
+                        Content = new StringContent("Podane zagadnienie nie istnieje", System.Text.Encoding.UTF8, "text/plain"),
+                        StatusCode = HttpStatusCode.NotFound
+                    };
+
+                    throw new HttpResponseException(response);
+                    //return NotFound();
+
+                }
+                if (!ModelState.IsValid)
+                {
+                    var response = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                    {
+                        Content = new StringContent("Nieprawidłowe żądanie", System.Text.Encoding.UTF8, "text/plain"),
+                        StatusCode = HttpStatusCode.BadRequest
+                    };
+                    throw new HttpResponseException(response);
+                    //return BadRequest("Nieprawidłowe żądanie");
+                }
+                return Ok(chosenNote);
+            } 
+            catch(Exception exc)
+            {
+                throw exc;
+            }    
         }
 
         [Route("dodaj-material")]
         [HttpPost]
         public IHttpActionResult AddNote([FromBody] Note newNote)
         {
-            if (!ModelState.IsValid)
+            try
             {
-                return BadRequest(ModelState);
+                if (!ModelState.IsValid)
+                {
+                    var response = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                    {
+                        Content = new StringContent("Nieprawidłowe żądanie", System.Text.Encoding.UTF8, "text/plain"),
+                        StatusCode = HttpStatusCode.BadRequest
+                    };
+                    throw new HttpResponseException(response);
+                    //return BadRequest(ModelState);
+                }
+                _dbContext.Notes.Add(newNote);
+                _dbContext.SaveChanges();
+
+                return Ok("Pomyślnie dodano materiał o id: " + newNote.Id);
             }
-            _dbContext.Notes.Add(newNote);
-            _dbContext.SaveChanges();
-            return Ok(newNote.Id);
+            catch(Exception exc)
+            {
+                throw exc;
+            }  
         }
 
         [Route("edycja-materialu/{id}")]
         [HttpPut]
         public IHttpActionResult UpdateNotes([FromUri] int id, [FromBody] Note modifiedNote)
         {
-            if (!ModelState.IsValid)
+            try
             {
-                return BadRequest("Nieprawidłowe żądanie");
-            }
-            var noteDb = _dbContext.Notes
-                .Where(s => s.Id == id).FirstOrDefault<Note>();
-            
-            if (noteDb != null)
-            {
-                noteDb.Title = modifiedNote.Title;
-                noteDb.Category = modifiedNote.Category;
-                noteDb.KeyWords = modifiedNote.KeyWords;
-                noteDb.Description = modifiedNote.Description;
-                noteDb.Link = modifiedNote.Link;
-                noteDb.Date = modifiedNote.Date;
-                noteDb.Author = modifiedNote.Author;
-                noteDb.University = modifiedNote.University;
-                noteDb.Email = modifiedNote.Email;
+                if (!ModelState.IsValid)
+                {
+                    var response = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                    {
+                        Content = new StringContent("Nieprawidłowe żądanie", System.Text.Encoding.UTF8, "text/plain"),
+                        StatusCode = HttpStatusCode.BadRequest
+                    };
+                    throw new HttpResponseException(response);
+                    //return BadRequest("Nieprawidłowe żądanie");
+                }
+                var noteDb = _dbContext.Notes
+                    .Where(s => s.Id == id).FirstOrDefault<Note>();
 
-                _dbContext.SaveChanges();
+                if (noteDb != null)
+                {
+                    noteDb.Title = modifiedNote.Title;
+                    noteDb.Category = modifiedNote.Category;
+                    noteDb.KeyWords = modifiedNote.KeyWords;
+                    noteDb.Description = modifiedNote.Description;
+                    noteDb.Link = modifiedNote.Link;
+                    noteDb.Date = modifiedNote.Date;
+                    noteDb.Author = modifiedNote.Author;
+                    noteDb.University = modifiedNote.University;
+                    noteDb.Email = modifiedNote.Email;
+
+                    _dbContext.SaveChanges();
+                }
+                else
+                {
+                    var response = new HttpResponseMessage(HttpStatusCode.NotFound)
+                    {
+                        Content = new StringContent("Podane zagadnienie nie istnieje", System.Text.Encoding.UTF8, "text/plain"),
+                        StatusCode = HttpStatusCode.NotFound
+                    };
+                    throw new HttpResponseException(response);
+                    //return NotFound();
+                }
+
+                return Ok("Zaktualizowano dane dotyczące materiału o id: " + id);
             }
-            else
+            catch (Exception exc)
             {
-                return NotFound();
-            }
-            
-            return Ok("Zaktualizowano dane dotyczące materiału o id: " + id);
+                //przy nieprawidłowym id robi catch
+                throw exc;
+            } 
         }
 
-     
         [Route("usun-material/{id}")]
         [HttpDelete]
         public IHttpActionResult Delete([FromUri] int id)
         {
-            var NoteToDelete = _dbContext.Notes.Find(id);
-            _dbContext.Notes.Remove(NoteToDelete);
-            _dbContext.SaveChanges();
-            return Ok("Usunięto dane");
+            try
+            {
+                var NoteToDelete = _dbContext.Notes.Find(id);
+
+                if (NoteToDelete == null)
+                {
+                    var response = new HttpResponseMessage(HttpStatusCode.NotFound)
+                    {
+                        Content = new StringContent("Podane zagadnienie nie istnieje", System.Text.Encoding.UTF8, "text/plain"),
+                        StatusCode = HttpStatusCode.NotFound
+                         
+                    };
+                    throw new HttpResponseException(response);
+                    //return NotFound();
+                }
+                if (!ModelState.IsValid)
+                {
+                    var response = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                    {
+                        Content = new StringContent("Nieprawidłowe żądanie", System.Text.Encoding.UTF8, "text/plain"),
+                        StatusCode = HttpStatusCode.BadRequest
+                    };
+                    throw new HttpResponseException(response);
+                    //return BadRequest("Nieprawidłowe żądanie");
+                }
+
+                _dbContext.Notes.Remove(NoteToDelete);
+                _dbContext.SaveChanges();
+                return Ok("Usunięto dane");
+            }
+            catch(Exception exc)
+            {
+                //przy nieprawidłowym id wywala catch
+                throw exc;
+            }
         }
     }
 }
